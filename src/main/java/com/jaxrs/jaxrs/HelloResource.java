4757a811ccd5ba70521024acaa44d7b439dc7651
unknown <jihed.mrouki@gmail.com>
2023-06-26 01:23:43 +0100

    todo

--- src/main/java/com/jaxrs/jaxrs/HelloResource.java
+++ src/main/java/com/jaxrs/jaxrs/HelloResource.java
@@ -1,14 +1,61 @@
 package com.jaxrs.jaxrs;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jdk.jfr.Event;
 
-@Path("/hello-world")
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/events")
 public class HelloResource {
-    @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    static List<Event> events = new ArrayList<Event>();
+    @POST
+    @Consumes(MediaType.APPLICATION_XML)
+    public String ajouterEvent(Event event) {
+        if (events.add(event))
+            return "Evenement ajoute";
+        else
+            return " Evenement non ajoute";
     }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)//"application/json"
+    public List<Event> getAllEvents()
+    {
+
+        return events;
+    }
+
+    @DELETE
+    @Path("events/idEvent/{id}")
+    public String deleteEvent(@PathParam("id") String idEvent) {
+        int index = this.getIndexOfEventById(idEvent);
+
+        if ((index != -1) && (events.remove(index) != null))
+            return "Evenement supprime";
+        return "Evenement non supprime";
+
+    }
+
+    @GET
+    @Path("{idEvent}")
+    @Produces("application/xml")
+    public Event chercherEmploye(@PathParam("idEvent") String idEvent) {
+        int index = this.getIndexOfEventById(idEvent);
+        if (index != -1) {
+            return events.get(index);
+        }
+        return null;
+    }
+
+    public int getIndexOfEventById(String idEvent) {
+        for (Event evt : events) {
+            if (evt.getId().equals(idEvent)) {
+                return events.indexOf(evt);
+            }
+        }
+        return -1;
+    }
+
 }
---
+++ src/main/java/com/jaxrs/jaxrs/event.java
@@ -0,0 +1,12 @@
+package com.jaxrs.jaxrs;
+
+public class event {
+
+    private Integer idEvent;
+
+    private String LibEvent;
+
+    private String description;
+
+    private String lieu;
+}
